a99b6dd807e5dc1f6e15bf032248e2c1169a5e07
konstantinos <kpetronikolos@gmail.com>
2019-03-11 21:44:05 +0200

    Add Data Context and the connection string for the Db.

---
+++ Data/Context/DataContext.cs
@@ -0,0 +1,19 @@
+﻿namespace Data.Context
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Core.Entities;
+    using Microsoft.EntityFrameworkCore;
+
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Make> Makes { get; set; }
+        public DbSet<Model> Models { get; set; }
+    }
+}
