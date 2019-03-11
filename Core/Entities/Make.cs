2539e96b2df3419df741ec5fcbf2c2d9bda0ecd0
konstantinos <kpetronikolos@gmail.com>
2019-03-11 21:22:06 +0200

    Add Model and Make Entities.

---
+++ Core/Entities/Make.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Core.Entities
+{
+    public class Make
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<Model> Models { get; set; }
+
+        public Make()
+        {
+            Models = new Collection<Model>();
+        }
+    }
+}
---
+++ Core/Entities/Model.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities
+{
+    public class Model
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Make Make { get; set; }
+
+        public int MakeId { get; set; }
+    }
+}
