e83968bed9bad48f37640c0977aad23bbcfdd4f9
konstantinos <kpetronikolos@gmail.com>
2019-03-16 20:23:41 +0200

    Seed Database.

---
+++ Data/Migrations/20190316173705_SeedDatabase.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class SeedDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Makes (Name) VALUES ('Make1')");
+            migrationBuilder.Sql("INSERT INTO Makes (Name) VALUES ('Make2')");
+            migrationBuilder.Sql("INSERT INTO Makes (Name) VALUES ('Make3')");
+
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('Make1-Model1', (SELECT Id FROM MAKES WHERE Name = 'Make1'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('Make1-Model2', (SELECT Id FROM MAKES WHERE NAME = 'Make1'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('Make1-Model3', (SELECT Id FROM MAKES WHERE NAME = 'Make1'))");
+
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('Make2-Model1', (SELECT Id FROM MAKES WHERE NAME = 'Make2'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('Make2-Model2', (SELECT Id FROM MAKES WHERE NAME = 'Make2'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('Make2-Model3', (SELECT Id FROM MAKES WHERE NAME = 'Make2'))");
+
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('Make3-Model1', (SELECT Id FROM MAKES WHERE NAME = 'Make3'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('Make3-Model2', (SELECT Id FROM MAKES WHERE NAME = 'Make3'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('Make3-Model3', (SELECT Id FROM MAKES WHERE NAME = 'Make3'))");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Makes WHERE Name IN ('Make1', 'Make2', 'Make3')");
+
+        }
+    }
+}
