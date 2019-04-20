19a245214be9768328f2c77ac2ee4ba6cf203f0f
konstantinos <kpetronikolos@gmail.com>
2019-04-20 15:52:32 +0300

    Create Features Controller.

---
+++ WebApi/Controllers/FeaturesController.cs
@@ -0,0 +1,27 @@
+﻿namespace WebApi.Controllers
+{
+    using Core.Entities;
+    using Data.Context;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    [Route("api/[controller]")]
+    public class FeaturesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public FeaturesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Feature>> GetFeatures()
+        {
+            return await _context.Features.ToListAsync();
+        }
+    }
+}
