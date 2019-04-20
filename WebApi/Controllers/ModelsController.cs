c26a844726c38ac416592b2fd58ea5c84fcd0c18
konstantinos <kpetronikolos@gmail.com>
2019-04-20 15:30:11 +0300

    Create Models Controller.

---
+++ WebApi/Controllers/ModelsController.cs
@@ -0,0 +1,33 @@
+﻿namespace WebApi.Controllers
+{
+    using AutoMapper;
+    using Core.Entities;
+    using Data.Context;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using WebApi.Resources;
+
+    [Route("api/[controller]")]
+    public class ModelsController : ControllerBase
+    {
+        private readonly DataContext _context;
+        private readonly IMapper _mapper;
+
+        public ModelsController(DataContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<ModelResource>> GetModels()
+        {
+            var models = await _context.Models.Include(m => m.Make).ToListAsync();
+
+            return _mapper.Map<List<Model>, List<ModelResource>>(models);
+        }
+    }
+}
