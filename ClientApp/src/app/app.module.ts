c4a63f4da03122019f84c7c741fd3927d66dc785
konstantinos <kpetronikolos@gmail.com>
2019-04-23 19:55:38 +0300

    Add Router.

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -2,15 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { NavmenuComponent } from './components/navmenu/navmenu.component';
+import { HomeComponent } from './components/home/home.component';
 
-@NgModule({
+@NgModule( {
   declarations: [
-    AppComponent
+    AppComponent,
+    NavmenuComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot( [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
+      { path: '**', redirectTo: 'home' }
+    ] )
   ],
   providers: [],
   bootstrap: [AppComponent]
-})
+} )
 export class AppModule { }
