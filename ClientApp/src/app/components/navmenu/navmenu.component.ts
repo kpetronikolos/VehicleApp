a7d7ab641c157498bb3ba621d7c10ea582531d08
konstantinos <kpetronikolos@gmail.com>
2019-04-23 19:55:18 +0300

    Add Menu and Home Component.

---
+++ ClientApp/src/app/components/navmenu/navmenu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component( {
+  selector: 'nav-menu',
+  templateUrl: './navmenu.component.html',
+  styleUrls: ['./navmenu.component.css']
+} )
+export class NavmenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
