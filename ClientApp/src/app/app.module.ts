9b6c9137711e642673feb262e4627a2022767857
konstantinos <kpetronikolos@gmail.com>
2019-04-23 20:07:44 +0300

    Add Form Vehicle Component and its Routing.

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -5,17 +5,20 @@ import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { NavmenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
+import { FormVehicleComponent } from './components/form-vehicle/form-vehicle.component';
 
 @NgModule( {
   declarations: [
     AppComponent,
     NavmenuComponent,
-    HomeComponent
+    HomeComponent,
+    FormVehicleComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot( [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'vehicle/new', component: FormVehicleComponent },
       { path: 'home', component: HomeComponent },
       { path: '**', redirectTo: 'home' }
     ] )
---
+++ ClientApp/src/app/components/form-vehicle/form-vehicle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component( {
+  selector: 'form-vehicle',
+  templateUrl: './form-vehicle.component.html',
+  styleUrls: ['./form-vehicle.component.css']
+} )
+export class FormVehicleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
