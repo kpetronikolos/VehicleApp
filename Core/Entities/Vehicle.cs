705c54591a47112ed6bb4821d328df7d0519aa09
konstantinos <kpetronikolos@gmail.com>
2019-04-26 17:15:38 +0300

    Add Vehicle Model.

---
+++ Core/Entities/Vehicle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Core.Entities
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+
+        public int ModelId { get; set; }
+        public Model Model { get; set; }
+
+        public bool IsRegistered { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string ContactName { get; set; }
+
+        [StringLength(255)]
+        public string ContactEmail { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string ContactPhone { get; set; }
+
+        public DateTime LastUpdate { get; set; }
+    }
+}
