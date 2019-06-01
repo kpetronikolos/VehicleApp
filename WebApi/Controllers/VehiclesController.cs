16470ea68b0c71f9416723b7b66a43e93b98a9d5
konstantinos <kpetronikolos@gmail.com>
2019-06-01 21:24:24 +0300

    Delete Vehicle.

--- WebApi/Controllers/VehiclesController.cs
+++ WebApi/Controllers/VehiclesController.cs
@@ -78,5 +78,21 @@
             return Ok(result);
         }
 
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVehicle(int id)
+        {
+            var vehicle = await _context.Vehicles.FindAsync(id);
+
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+
+            _context.Remove(vehicle);
+            await _context.SaveChangesAsync();
+
+            return Ok(id);
+        }
+
     }
 }
