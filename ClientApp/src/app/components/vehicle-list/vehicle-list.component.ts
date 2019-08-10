858dec378ed8cde749184122fb8bd72c6ec56f05
konstantinos <kpetronikolos@gmail.com>
2019-08-10 20:41:33 +0300

    Client side filtering.

--- ClientApp/src/app/components/vehicle-list/vehicle-list.component.ts
+++ ClientApp/src/app/components/vehicle-list/vehicle-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { VehicleResource } from '../../resources/VehileResource';
 import { VehicleService } from '../../services/vehicle.service';
+import { Make } from '../../models/Make';
 
 @Component( {
   selector: 'app-vehicle-list',
@@ -10,14 +11,33 @@ import { VehicleService } from '../../services/vehicle.service';
 export class VehicleListComponent implements OnInit {
 
   public vehicles: VehicleResource[];
+  public allVehicles: VehicleResource[];
+  public makes: Make[];
+
+  public filter: any = {};
 
   constructor( private vehicleService: VehicleService ) { }
 
   ngOnInit() {
+    this.vehicleService.getMakes()
+      .subscribe( makes => {
+        this.makes = makes
+      } );
+
     this.vehicleService.getVehicles()
       .subscribe( data => {
-        this.vehicles = data;
+        this.vehicles = this.allVehicles = data;
       } )
+  }
+
+  public onFilterChange(): void {
+    var filteredVehicles = this.allVehicles;
+
+    if ( this.filter.makeId ) {
+      filteredVehicles = filteredVehicles.filter( v => v.make.id == this.filter.makeId )
+    }
+
+    this.vehicles = filteredVehicles;
   }
 
 }
